2096665fdaf59092a7e90396e2ef1e04e93d55af
Howlingdaemon <144964738+Howlingdaemon@users.noreply.github.com>
2023-09-19 07:52:27 -0500

    Add project files.

---
+++ MyFirstProject/MyFirstProject.cpp
@@ -0,0 +1,14 @@
+/*
+    Mason Newell
+    2023=2024
+    Due: Sept. 20th 2023
+    Lab 1 - Mason Newell
+    in this lab i will output text into a command prompt saying "Hello World! My favorite color is Green!"
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World! My favorite color is Green!\n";
+}
